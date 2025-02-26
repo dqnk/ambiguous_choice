8f821e02808000e1d10e08bfbe6f8a3a03825ba9
dqnk <64268180+dqnk@users.noreply.github.com>
2025-02-26 20:53:36 +0100

    add comments

--- ambiguous_choice.ml
+++ ambiguous_choice.ml
@@ -3,14 +3,14 @@ module Task = Domainslib.Task;;
 Random.self_init ();;
 
 let f () =
-  let top = Random.int 1000_000 in
+  let top = Random.int 1_000_000 in
   let x = ref 0 in
   for _ = 0 to top do
     x := !x + 1;
   done;
-  !x < 100;;
+  !x < 1000;;
 
-(* Helper function to verify what f does *)
+(* Helper function to verify what f does, mostly irrelevant *)
 let single_thread_verify () =
   let cnt = ref 0 in
   for _ = 0 to 10_000 do
@@ -21,26 +21,27 @@ let single_thread_verify () =
   done;
   print_int !cnt;;
 
+(* return result and remaining lifespan *)
 let rec run_test lifespan = match lifespan with
-  | 0 -> false
-  | _ -> if f () then true else run_test (lifespan - 1)
+  | 0 -> false, 0
+  | _ -> if f () then true, lifespan else run_test (lifespan - 1)
 
 let single_thread_test lifespan =
   run_test lifespan;;
 
 (* convert return values to option for Task.parallel_find *)
-let maybe_of_bool b = match b with
-  | false -> None
-  | true -> Some true;;
+let wrap b = match b with
+  | false, _ -> None
+  | true, x -> Some (true, x);;
 
 
 (* Convert back to bool - true if any of the threads had found the needle and false if none did *)
 let search_needle pool lifespan = match
   Task.parallel_find pool ~start:0 ~finish:(lifespan) ~body:(fun _ ->
-    maybe_of_bool (run_test 1)
+    wrap (run_test 1)
   ) with
-    | Some _ -> true
-    | None -> false;;
+    | Some (true, x) -> true, x
+    | _ -> false, 0;;
 
 let multi_thread_test lifespan num_domains =
   let pool = Task.setup_pool ~num_domains ~name:"pool" () in
@@ -48,9 +49,12 @@ let multi_thread_test lifespan num_domains =
   Task.teardown_pool pool;
   res;;
 
-(*
 print_string "Single thread:\n";
-print_string (string_of_bool (single_thread_test 1_000_000));
-*)
+let result = single_thread_test 1_000_000 in
+print_string (string_of_bool (fst result) ^ " " ^ string_of_int (snd result));
+
+print_newline ();
+
 print_string "Multi thread:\n";
-print_string (string_of_bool (multi_thread_test 1000_000 8));
+let result = multi_thread_test 1_000_000 4 in
+print_string (string_of_bool (fst result) ^ " " ^ string_of_int (snd result));
--- ambiguous_choice.mli
+++ ambiguous_choice.mli
@@ -1,3 +1,3 @@
-val multi_thread_test: int -> int -> bool
-val single_thread_test: int -> bool
+val multi_thread_test: int -> int -> bool * int
+val single_thread_test: int -> bool * int
 val single_thread_verify: unit -> unit
