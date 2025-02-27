8859c3165b4e2b680a4f87cc19520532264c7c8c
dqnk <64268180+dqnk@users.noreply.github.com>
2025-02-27 09:56:44 +0100

    formatting, renaming, cleanup

--- ambiguous_choice.ml
+++ ambiguous_choice.ml
@@ -1,60 +1,64 @@
 module Task = Domainslib.Task;;
 
-Random.self_init ();;
+Random.self_init ()
 
 let f () =
-  let top = Random.int 1_000_000 in
+  let rnd = Random.int 1_000_000 in
   let x = ref 0 in
-  for _ = 0 to top do
-    x := !x + 1;
+  for _ = 0 to rnd do
+    x := !x + 1
   done;
-  !x < 1000;;
+  (* return a boolean value which is rarely true and the value of x for debugging *)
+  (!x < 1000, !x)
 
-(* Helper function to verify what f does, mostly irrelevant *)
+(* helper function to verify what f does, mostly irrelevant *)
 let single_thread_verify () =
   let cnt = ref 0 in
   for _ = 0 to 10_000 do
-    if f () then
-      begin
-        cnt := !cnt + 1;
-        end
+    let res = f () in
+    if fst res then cnt := !cnt + 1
   done;
-  print_int !cnt;;
+  print_int !cnt
 
-(* return result and remaining lifespan *)
-let rec run_test lifespan = match lifespan with
-  | 0 -> false, 0
-  | _ -> if f () then true, lifespan else run_test (lifespan - 1)
+(* return result and remaining fuel *)
+let rec run_test fuel =
+  let res, rnd = f () in
+  match fuel with
+  | 0 -> (false, 0)
+  (* you could also return fuel instead of rnd if you wanted for debugging purposes *)
+  | _ -> if res then (true, rnd) else run_test (fuel - 1)
 
-let single_thread_test lifespan =
-  run_test lifespan;;
+let single_thread_test ~fuel = run_test fuel
 
 (* convert return values to option for Task.parallel_find *)
-let wrap b = match b with
-  | false, _ -> None
-  | true, x -> Some (true, x);;
+let wrap b = match b with false, _ -> None | true, x -> Some (true, x)
 
+(* convert back to bool - true if any of the threads had found the needle and false if none did *)
+let search_needle pool fuel =
+  match
+    Task.parallel_find pool ~start:0 ~finish:fuel ~body:(fun _ ->
+        wrap (run_test 1))
+  with
+  | Some (true, x) -> (true, x)
+  | _ -> (false, 0)
 
-(* Convert back to bool - true if any of the threads had found the needle and false if none did *)
-let search_needle pool lifespan = match
-  Task.parallel_find pool ~start:0 ~finish:(lifespan) ~body:(fun _ ->
-    wrap (run_test 1)
-  ) with
-    | Some (true, x) -> true, x
-    | _ -> false, 0;;
-
-let multi_thread_test lifespan num_domains =
+let multi_thread_test ~fuel ~num_domains =
   let pool = Task.setup_pool ~num_domains ~name:"pool" () in
-  let res = Task.run pool (fun () -> search_needle pool lifespan) in
+  let res = Task.run pool (fun () -> search_needle pool fuel) in
   Task.teardown_pool pool;
-  res;;
+  res
+
+let fuel = 1_000_000
+let num_domains = 4;;
 
 print_string "Single thread:\n";
-let result = single_thread_test 1_000_000 in
+let result = single_thread_test ~fuel in
 print_string (string_of_bool (fst result) ^ " " ^ string_of_int (snd result));
 
 print_newline ();
 
 print_string "Multi thread:\n";
-let result = multi_thread_test 1_000_000 4 in
+let result = multi_thread_test ~fuel ~num_domains in
 print_string (string_of_bool (fst result) ^ " " ^ string_of_int (snd result));
+
+print_newline ()
--- ambiguous_choice.mli
+++ ambiguous_choice.mli
@@ -1,3 +1,3 @@
-val multi_thread_test: int -> int -> bool * int
-val single_thread_test: int -> bool * int
-val single_thread_verify: unit -> unit
+val multi_thread_test : fuel:int -> num_domains:int -> bool * int
+val single_thread_test : fuel:int -> bool * int
+val single_thread_verify : unit -> unit
